be2431e07f6e7577b6844bb4fed33e5cd2d8ed5d
Colin McGovern <thecolinmcgovern@gmail.com>
2026-07-23 13:51:00 -0400

    25 tests pass

--- ArcDriver.py
+++ ArcDriver.py
@@ -92,11 +92,11 @@ if __name__ == "__main__":
     # TEST_PROBLEM_HASH = ["81c0276b", "9af7a82c", "c8b7cc0f"] # Make Graph
     # TEST_PROBLEM_HASH = ["18419cfa", "f8a8fe49"] # Blob Reflections
     # TEST_PROBLEM_HASH = ["d931c21c", "ce22a75a"] # Dialate Inscribe
-    TEST_PROBLEM_HASH = ["4b6b68e5", "7b6016b9", "b2862040"] # Donuts
+    # TEST_PROBLEM_HASH = ["4b6b68e5", "7b6016b9", "b2862040"] # Donuts
     # TEST_PROBLEM_HASH = ["f76d97a5", "ed36ccf7", "b1948b0a", "1cf80156"] # General
 
     # TEST_PROBLEM_HASH = ["c48954c1"]
-    # TEST_PROBLEM_HASH = None
+    TEST_PROBLEM_HASH = None
 
     arc_milestone_problems: list[ArcProblem] = []
     for milestone_letter in ('B', 'C', 'D'):
